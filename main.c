da41dc96faea2119d974f6cf2879c41897bcd0c7
Valeri Buntashova <vbuntashova@mail.ru>
2018-05-29 22:04:51 +0700

    split on funcs

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #define WORD_FILES_PATH "./words/"
 #define MAX_CATEGORIES  4
 
+char* select_word = NULL;
+char* hidden_word = NULL;
+
 enum categories {
 	animals = 0,
 	computers,
@@ -14,35 +17,49 @@ enum categories {
 	music
 };
 
-
-int main(void)
-{
+char * select_categorie() {
 	int categor = 0;
-	char file_name[20] = {0};
+	char tmp[20] = {0};
 	srand(time(NULL));
 	categor = (int)(rand()) % MAX_CATEGORIES;
-	FILE *f = NULL;
 	switch (categor) {
 	case animals:
-		sprintf(file_name, "%sanimals", WORD_FILES_PATH);
+		sprintf(tmp, "%sanimals", WORD_FILES_PATH);
 		printf("\nYour category of game: ANIMALS\n\n");
 		break;
 	case computers:
-		sprintf(file_name, "%scomputers", WORD_FILES_PATH);
+		sprintf(tmp, "%scomputers", WORD_FILES_PATH);
 		printf("\nYour category of game: COMPUTERS\n\n");
 		break;
 	case home:
-		sprintf(file_name, "%shome", WORD_FILES_PATH);
+		sprintf(tmp, "%shome", WORD_FILES_PATH);
 		printf("\nYour category of game: HOME\n\n");
 		break;
 	case music:
-		sprintf(file_name, "%smusic", WORD_FILES_PATH);
+		sprintf(tmp, "%smusic", WORD_FILES_PATH);
 		printf("\nYour category of game: MUSIC\n\n");
 		break;
 	default:
 		printf("ERROR!\n");
 		break;
 	}
+	int len = strlen(tmp);
+	if (len < 0)
+	{
+		return NULL;
+	}
+	char *file_name = malloc(len * sizeof(char));
+	if (!file_name) {
+		printf("ERROR malloc");
+		return NULL;
+	}
+	memset(file_name, 0, len);
+	memcpy(file_name, tmp, len);
+	return file_name;
+}
+
+int get_word(char *file_name) {
+	FILE *f = NULL;
 	if ( ( f = fopen( file_name, "r" ) ) == NULL ) {
 		printf( "Can not open file %s\n", file_name );
 		return -1;
@@ -64,42 +81,59 @@ int main(void)
 	char tmp[32] = {0};
 	fscanf(f, "%s", tmp);
 	printf("%s\n", tmp);
-
 	int len = strlen(tmp);
-	char *word = malloc(len * sizeof(char));
-	char *hidden = malloc(len * sizeof(char));
-	for (int i = 0; i < len; ++i)
+	select_word = malloc(len * sizeof(char));
+	hidden_word = malloc(len * sizeof(char));
+	if (select_word == NULL || hidden_word == NULL)
 	{
-		hidden[i] = '*';
-	}
-	if (word == NULL)
-	{
-		printf("ERROR\n");
+		printf("ERROR malloc\n");
+		fclose(f);
 		return -1;
 	}
-	memcpy(word, tmp, len);
+	for (int i = 0; i < len; ++i)
+	{
+		hidden_word[i] = '*';
+	}
+	memcpy(select_word, tmp, len);
+	fclose(f);
+	return len;
+}
+
+int check_letter(char ch)
+{
+	if (('a' <= ch && ch <= 'z'))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int game(int word_len)
+{
 	int cur_miss = 0;
 	int leter = 0;
 	int hidden_len = 0;
 	while (cur_miss < GAME_MISS)
 	{
 		int flg = 0;
-		printf("\nHidden word [%s]\n", hidden);
+		printf("\nHidden word [%s]\n", hidden_word);
 		printf("Input leters: ");
 		leter = fgetc(stdin);
 		fgetc(stdin); // for /n
-		if (('a' <= leter && leter <= 'z') || ('A' <= leter && leter <= 'Z'))
+		fflush(stdin);
+		if (check_letter(leter) == 1)
 		{
-			for (int i = 0; i < len; ++i)
+			for (int i = 0; i < word_len; ++i)
 			{
-				if (word[i] == leter && hidden[i] != leter)
+				if (select_word[i] == leter && hidden_word[i] != leter)
 				{
+
 					flg = 1;
-					hidden[i] = leter;
+					hidden_word[i] = leter;
 					hidden_len++;
-					if (hidden_len == len)
+					if (hidden_len == word_len)
 					{
-						printf("\n                YOU WIN!!!! \n                THIS IS WORD : %s\n\n", hidden);
+						printf("\nYOU WIN!!!! %s\n", hidden_word);
 						return 0;
 					}
 					break;
@@ -107,14 +141,38 @@ int main(void)
 			}
 			if (flg == 0)
 			{
-				printf("\nLetter - '%c' not found! Try again\n",leter);
+				printf("\nLetter - '%c' not found! Try again\n", leter);
 				cur_miss++;
 			}
 		}
-		else{
+		else {
 			printf("\nINVALID CHARACTER! \nTRY AGAIN \n");
 		}
 	}
-	printf("\n                YOU LOSS\n\n");
+	printf("\nYOU LOSS\n");
+	return 0;
+}
+
+int main(void)
+{
+
+
+	char *file_name = NULL;
+	file_name = select_categorie();
+	if (file_name == NULL)
+	{
+		printf("ERROR");
+		return -1;
+	}
+	int len = get_word(file_name);
+	if (len < 0)
+	{
+		printf("Error");
+		return -1;
+	}
+	game(len);
+	free(file_name);
+	free(select_word);
+	free(hidden_word);
 	return 0;
 }
